dd90fb04ca7cde21ce92c37062b0d1225e42af52
Angelillo15 <contact@angelillo15.es>
2024-11-20 13:40:02 +0000

    2024/1 initial repo

---
+++ 2024/open/1/java/Main.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+
+
+
+public class Main {
+  private static final Scanner scanner = new Scanner(System.in);
+
+  public static final void main(String[] args) {
+    int max = scanner.nextInt();
+    System.out.println("Buenos dias");    
+  }
+
+  public static final void fibbonnaci()
+}
