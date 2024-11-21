1a8f6f0f9ad422b8a999a395c76e81a6b51412de
Angelillo15 <contact@angelillo15.es>
2024-11-21 09:11:56 +0000

    bump

--- 2024/open/1/java/Main.java
+++ 2024/open/1/java/Main.java
@@ -1,15 +1,25 @@
 
+import java.util.Arrays;
 import java.util.Scanner;
-
-
 
 public class Main {
   private static final Scanner scanner = new Scanner(System.in);
 
   public static final void main(String[] args) {
+    System.out.print("Ingrese el numero maximo: ");
     int max = scanner.nextInt();
-    System.out.println("Buenos dias");    
+    System.out.println(Arrays.toString(fibbonnaci(max)));
   }
 
-  public static final void fibbonnaci()
+  public static final int[] fibbonnaci(final int max) {
+    final int[] fibbonnaci = new int[max];
+    fibbonnaci[0] = 0;
+    fibbonnaci[1] = 1;
+
+    for (int i = 2; i < max; i++) {
+      fibbonnaci[i] = fibbonnaci[i - 1] + fibbonnaci[i - 2];
+    }
+
+    return fibbonnaci;
+  }
 }
